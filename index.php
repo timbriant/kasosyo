fdc6cc4dba433b94573a8f061f10cbc7918e15da
Tim Coñejos <Tim Coejos>
2017-07-22 14:29:08 +0800

    main page added

--- index.php
+++ index.php
@@ -1,8 +1,22 @@
 <html>
     <head>
-    <title> Kasosyo - Website for Sari-Sari Stores</title>
-    </head>
+        <link rel = "stylesheet" href = "css/bootstrap.min.css"/>
+        <link rel = "stylesheet" href = "css/bootstrap-theme.min.css"/>
+        <link rel = "stylesheet" href = "css/styles.css"/>
+        <title> Kasosyo - Website for Sari-Sari Stores</title>
+    </head> 
     <body>
-            
+          <nav class = "navbar navbar-default navbar-fixed-top">
+            <div class = "container-fluid">
+                <div class = "navbar-header">
+                    <a class = "navbar-brand">Kasosyo</a>
+                </div>
+                <ul class = "nav navbar-nav navbar-right">
+                    <li> <a href = "#">Login</a>
+                    <li> <a href = "#">Sign Up</a>
+                </ul>
+            </div>
+          </nav>
     </body>
+    <script src = "js/bootstrap.min.js"/>
 </html>
